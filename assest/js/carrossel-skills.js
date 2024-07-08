698988b2f8c08b7f787b960cfd74494b8ae74d35
liihAbreu <49107493+liihAbreu@users.noreply.github.com>
2024-07-08 18:21:09 -0300

    Ajustes finos

--- assest/js/carrossel-skills.js
+++ assest/js/carrossel-skills.js
@@ -4,8 +4,8 @@ $('.carrosseul-skills').slick({
     slidesToScroll: 3,
     centerMode: true,
     dots:true,
-    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="/assest/img/skills/arrow-left.png" alt="para esquerda"></button>',
-    nextArrow: '<button class="slick-next slick-arrow" aria-label="Previous" type="button" style=""><img src="/assest/img/skills/arrow-right.png" alt="para direita"></button>',
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button" style=""><img src="./assest/img/skills/arrow-left.png" alt="para esquerda"></button>',
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Previous" type="button" style=""><img src="./assest/img/skills/arrow-right.png" alt="para direita"></button>',
     autoplay: true,
     responsive: [
         {
